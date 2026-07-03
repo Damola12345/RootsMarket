a87c01c4fd083c98c85c03465e0dfdd16f5c5ef7
Damola12345 <ajiboladamola789@gmail.com>
2026-07-03 02:50:19 +0100

    update database schema and seed data

---
+++ infra/db/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO products (name, price, stock)
+VALUES
+('Garri', 5000, 100),
+('Elubo', 6000, 100),
+('Yam Flour', 7000, 100),
+('Palm Oil', 8000, 100),
+('Honey', 12000, 100),
+('Ori (Shea Butter)', 4000, 100);
